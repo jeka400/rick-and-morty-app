d3466cda4f36e64249e31158cbc36571f287737a
jeka400 <jelenakrstic164@gmail.com>
2025-01-21 18:56:32 +0100

    Add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login';
+import { Container } from 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-    </div>
+    <Container>
+      <Login />
+    </Container>
   );
 }
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { auth } from "../firebase";
+
+const Login: React.FC = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return (
+        <Form onSubmit={ handleSubmit }>
+            <h1 className="mb-5 mt-5 text-center">Login</h1>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" value={ email } onChange={ (e) => setEmail(e.target.value) } />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter password" value={ password } onChange={ (e) => setPassword(e.target.value) }/>
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="mb-3">
+                Submit
+            </Button>
+
+            <hr />
+
+            <p>Don't have an account? </p>
+        </Form>
+    )
+}
+
+export default Login;
