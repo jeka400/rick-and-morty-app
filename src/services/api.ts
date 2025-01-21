b4d7021993f3023021b75da5c3142283eee05651
jeka400 <jelenakrstic164@gmail.com>
2025-01-21 22:51:02 +0100

    Add API module with React Query hooks

---
+++ src/services/api.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const apiClient = axios.create({
+    baseURL: "https://rickandmortyapi.com/api/",
+});
+
+
+export const useCharacters = (page: number) => {
+    return useQuery(["characters", page]), async () => {
+        const { data } = await apiClient.get(`/character?page=${page}`);
+        return data;
+    }
+}
+
+export const useCharacter = (id: string | undefined) => {
+    return useQuery(["character", id], async () => {
+        if (!id) throw new Error("No ID provided");
+
+        const { data } = await apiClient.get(`/character/${id}`);
+        return data;
+      },
+      {
+        enabled: !!id,
+      }
+    );
+};
+
+export const useLocation = (id: number) => {
+    return useQuery(["location", id]), async () => {
+        const { data } = await apiClient.get(`/location/${id}`);
+        return data;
+    }
+}
+
+export const useEpisode = (id: number) => {
+    return useQuery(["episode", id]), async () => {
+        const { data } = await apiClient.get(`/episode/${id}`);
+        return data;
+    }
+}
+
+
+export default apiClient;
