57fb1043e337b59156c9bb08a3c0ff69cdada4eb
jeka400 <jelenakrstic164@gmail.com>
2025-01-22 23:36:21 +0100

    Marked routes as private or public based on authentication requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "react-query";
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Characters from './pages/Characters';
 import SingleCharacter from './pages/SingleCharacter';
 import SingleLocation from './pages/SingleLocation';
 import SingleEpisode from './pages/SingleEpisode';
+
+interface IPrivateRouteProps  {
+  children: React.ReactNode
+}
+
+const PrivateRoute: React.FC<IPrivateRouteProps> = ({ children }) => {
+  const { user } = useAuth();
+
+  return user ? children : <Link to="/login" />
+}
 
 const App: React.FC = () => {
   const queryClient = new QueryClient({
@@ -25,15 +35,38 @@ const App: React.FC = () => {
       <QueryClientProvider client={queryClient}>
         <Router>
           <Routes>
+
             <Route path='/login' element={ <Login /> } />
+
             <Route path='/signup' element={ <Signup />} />
-            <Route path='/characters' element={ <Characters /> }/>
-            <Route path='/characters/:id' element={ <SingleCharacter /> } />
-            <Route path='/location/:id' element={ <SingleLocation /> } />
-            <Route path='/episode/:id' element={ <SingleEpisode /> } />
+
+            <Route path='/characters' element={
+              <PrivateRoute>
+                <Characters />
+              </PrivateRoute> 
+            }/>
+
+            <Route path='/characters/:id' element={ 
+              <PrivateRoute>
+                <SingleCharacter /> 
+              </PrivateRoute> 
+            } />
+
+            <Route path='/location/:id' element={ 
+              <PrivateRoute>
+                <SingleLocation /> 
+              </PrivateRoute> 
+            } />
+
+            <Route path='/episode/:id' element={ 
+              <PrivateRoute>
+                <SingleEpisode /> 
+              </PrivateRoute>
+            } />
+
           </Routes>
         </Router>
-        </QueryClientProvider>
+      </QueryClientProvider>
     </AuthProvider>
   );
 }
